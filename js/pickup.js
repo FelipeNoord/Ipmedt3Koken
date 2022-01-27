b612cb6be79b274813b8d056d664866f5c7e8ea4
tomlommerse <tomlommerse2001@gmail.com>
2022-01-27 17:08:16 +0100

    klit

--- js/pickup.js
+++ js/pickup.js
@@ -107,32 +107,39 @@
 
         for (let i = 0; i < placeholders.length; i++) {
           placeholders[i].addEventListener("click", function(evt){  //placeholders[i] is een array van alle elementen die als class js--placeholder hebben
-            if(hold == "true" && pickups[i].classList.contains("burger")){
-              // als hetgeen wat is opgepakt als class "burger" heeft voert het deze if uit
 
-              let box = document.createElement("a-cylinder")
-              box.setAttribute("class", "js--pickup js--interact burger");
-              box.setAttribute("color", pickups[i].getAttribute("color"));
-              box.setAttribute("height", pickups[i].getAttribute("height"));
-              box.setAttribute("radius",  pickups[i].getAttribute("radius"))
-              box.setAttribute("position", {x: this.getAttribute("position").x, y:"0.5", z: this.getAttribute("position").z });
-              //hierboven wordt een nieuwe element gemaakt dat de attributen van hetgeen wat je vast hebt kopieert.
-              scene.appendChild(box); // hier word het nieuwe element toegevoegd aan de scene
-              document.getElementById("js--hold").remove(); // het element met id "js--hold" word verwijderd, dus het element in de camera word verwijderd
-              addListeners();
-              hold = null; //hold weer op null
-            }else if(hold == "true" && pickups[i].classList.contains("tomaat")){
-              let box = document.createElement("a-sphere")
-              box.setAttribute("class", "js--pickup js--interact tomaat");
-              box.setAttribute("color", pickups[i].getAttribute("color"));
-              box.setAttribute("height", pickups[i].getAttribute("height"));
-              box.setAttribute("radius",  pickups[i].getAttribute("radius"))
-              box.setAttribute("position", {x: this.getAttribute("position").x, y:"0.5", z: this.getAttribute("position").z });
-              scene.appendChild(box);
-              document.getElementById("js--hold").remove();
-              addListeners();
-              hold = null;
+
+            for (let j = 0; i < pickups.length; j++){
+              if(hold == "true" && pickups[j].classList.contains("burger")){
+                // als hetgeen wat is opgepakt als class "burger" heeft voert het deze if uit
+  
+                let box = document.createElement("a-entity")
+                //<a-entity pickup class="js--interact js--pickup burger" id="burger" gltf-model="#burger-glb" position="0.5 1.14 1.4" scale="0.3 0.3 0.3"></a-entity>
+                box.setAttribute("class", "js--pickup js--interact burger");
+                box.setAttribute("id", "burger");
+                box.setAttribute("gltf-model", "#burger-glb");
+                box.setAttribute("scale",  "0.3 0.3 0.3")
+                box.setAttribute("position", {x: this.getAttribute("position").x, y:"1.177", z: this.getAttribute("position").z });
+                console.log(box)
+                //hierboven wordt een nieuwe element gemaakt dat de attributen van hetgeen wat je vast hebt kopieert.
+                scene.appendChild(box); // hier word het nieuwe element toegevoegd aan de scene
+                document.getElementById("js--hold").remove(); // het element met id "js--hold" word verwijderd, dus het element in de camera word verwijderd
+                addListeners();
+                hold = null; //hold weer op null
+              }else if(hold == "true" && pickups[j].classList.contains("tomaat")){
+                let box = document.createElement("a-sphere")
+                box.setAttribute("class", "js--pickup js--interact tomaat");
+                box.setAttribute("color", pickups[j].getAttribute("color"));
+                box.setAttribute("height", pickups[j].getAttribute("height"));
+                box.setAttribute("radius",  pickups[j].getAttribute("radius"))
+                box.setAttribute("position", {x: this.getAttribute("position").x, y:"1.14", z: this.getAttribute("position").z });
+                scene.appendChild(box);
+                document.getElementById("js--hold").remove();
+                addListeners();
+                hold = null;
+              }
             }
+
           });
           }
           
