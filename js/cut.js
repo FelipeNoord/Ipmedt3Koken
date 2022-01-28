d3cc2fd533af64ad5c7f261ee705a5601157c0b8
1108188 <s1108188@student.hsleiden.nl>
2022-01-28 02:50:45 +0100

    again added cut animation

--- js/cut.js
+++ js/cut.js
@@ -5,25 +5,25 @@ AFRAME.registerComponent('cut', {
         const cuttable = document.getElementsByClassName("js--cuttable");
         let hold = "knife";
         const tomato = document.getElementById("js--tomato");
-        const knife = '<a-entity gltf-model="#chefsmes-glb" id="js--knife" rotation="-45 80 50" position="0.3 -0.2 -0.3" scale="0.1 0.1 0.1"></a-entity>';
+        const knife = '<a-entity gltf-model="#chefsmes-glb" id="js--cuttingknife" rotation="0 -90 0" position="0.3 -0.2 -0.3" scale="0.1 0.1 0.1"></a-entity>';
         const bun = document.getElementById("js--bun");
 
-        bun.eventListener("click", function(evt){
-          if(hold === "knife" && bun.getAttribute("id") == "js--bun"){
-            camera.children[1].remove();
-            console.log("hallo");
-            
-            bun.outerHTML += knife;
-            const cutKnife = document.getElementById("js--knife");
-            cutKnife.setAttribute('animation__pos', "property: position; from: 1.2 1.20 0.25; to: 0.8 1.20 0.25; dur: 1000;");
-            cutKnife.setAttribute("animation__rot", "property: rotation; from: 0 -90 0; to: 0 -90 30; dur: 200; loop:5");
-            
-            setTimeout(returnKnife = () =>{
-              cutKnife.setAttribute();
-            }, )
-          }
-        });
-    
+
+        function addListeners() {
+          bun.addEventListener("click", function(evt){
+            console.log(bun.getAttribute("position"));
+            if(bun.getAttribute("id") == ("js--bun")){
+              bun.outerHTML += knife;
+
+              let knifeAnimations = document.getElementById("js--cuttingknife");
+
+              knifeAnimations.setAttribute("animation__pos" , "property: position; from: 0.8 1.25 0.3; to: 0.4 1.25 0.3; dur: 1000");
+              knifeAnimations.setAttribute("animation__rot" , "property: rotation; from: 0 -90 30; to: 0 -90 0; loop: 4; dur: 250; easing: linear;");
+            }
+          });
+        }
+        
+    addListeners();
 
 
     },
--- js/pickup.js
+++ js/pickup.js
@@ -7,6 +7,7 @@
         const camera = document.getElementById("js--camera");
         const tomato = document.getElementById("js--tomato");
         const knife = document.getElementById("js--knife");
+        const knifeP = '<a-entity cut class="js--interact js--pickup" id="js--cut" gltf-model="#chefsmes-glb" metalness: 1; roughness: 0; rotation="-30 90 20" position="0.2 -0.2 -0.4" scale="0.1 0.1 0.1"></a-entity>'
       
         let hold = null;
         console.log("hallo");
@@ -17,13 +18,23 @@
         
         for (let i = 0; i < pickups.length; i++) {
         pickups[i].addEventListener("click", function(evt){ //pickups is een array met alle elementen die als class "pickup" hebben
-          if(pickups[i].id == "broodje"){
-             document.getElementById("broodje").setAttribute("position", "0.6 1.17 0.6");
+          if(pickups[i].id == "js--bun"){
+             document.getElementById("js--bun").setAttribute("position", "0.6 1.17 0.6");
           }
+
+        
 
 
       });
         }
+
+        knife.addEventListener("click", function(evt){
+          if(hold == null && knife.getAttribute("id") == "js--knife"){
+            console.log("hallo");
+            this.remove();
+            camera.innerHTML += knifeP; 
+          }
+        })
       }
 
       addListeners();
