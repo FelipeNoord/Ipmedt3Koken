7c96dbb922de1165798be68936a6d129da99211b
Serilu <redkirbyog@gmail.com>
2022-01-28 02:29:33 +0100

    Stap toegevoegd en animations

--- js/pickup.js
+++ js/pickup.js
@@ -20,30 +20,41 @@
         var j = 0;
         for (let i = 0; i < pickups.length; i++) {
         pickups[i].addEventListener("click", async function(evt){ //pickups is een array met alle elementen die als class "pickup" hebben
-          if(pickups[i].id == "burger" && j == 0){
-            document.getElementById("burger").setAttribute("position", "0.5 1.14 -1");
+
+          if(pickups[i].id == "olie" && j == 0){
+            document.getElementById("olie").setAttribute("position", "0.53 2 -1");
+            document.getElementById("olie").setAttribute("rotation", "180 0 0");
+            document.getElementById("olie").setAttribute("animation", "property: position; from: 0.53 2.2 -1; to: 0.53 2 -1; dur: 2000; easing: linear; loop: 2");
+            await sleep(4000);
+            document.getElementById("olie").setAttribute("position", "0.6 1.14 1.4");
+            document.getElementById("olie").setAttribute("rotation", "0 0 0");
             j++;
          }
-          if(pickups[i].id == "broodje" && j == 1){
+
+          if(pickups[i].id == "burger" && j == 1){
+            document.getElementById("burger").setAttribute("position", "0.53 1.19 -1");
+            j++;
+         }
+          if(pickups[i].id == "broodje" && j == 2){
              document.getElementById("broodje").setAttribute("position", "0.6 1.17 0.6");
              j++;
           }
-          if(pickups[i].id == "broodje" && j == 2){
+          if(pickups[i].id == "broodje" && j == 3){
             document.getElementById("broodje").setAttribute("position", "-0.5 1.17 0.55");
             j++;
-          }if(pickups[i].id == "slablaadje" && j == 3){
+          }if(pickups[i].id == "slablaadje" && j == 4){
             document.getElementById("slablaadje").setAttribute("position", "-0.5 1.2 0.55");
             j++;
-          }if(pickups[i].id == "tomaat"  && j == 4){
+          }if(pickups[i].id == "tomaat"  && j == 5){
             document.getElementById("tomaat").setAttribute("position", "0.6 1.17 0.6");
             j++;
-          }if(pickups[i].id == "tomaat"  && j == 5){
+          }if(pickups[i].id == "tomaat"  && j == 6){
             document.getElementById("tomaat").setAttribute("position", "-0.5 1.2 0.55");
             j++;
-          }if(pickups[i].id == "burger"  && j == 6){
+          }if(pickups[i].id == "burger"  && j == 7){
             document.getElementById("burger").setAttribute("position", "-0.5 1.2 0.55");
             j++;
-          }if(pickups[i].id == "ketchup"  && j == 7){
+          }if(pickups[i].id == "ketchup"  && j == 8){
             document.getElementById("ketchup").setAttribute("position", "-0.5 2 0.55");
             document.getElementById("ketchup").setAttribute("rotation", "180 0 0");
             document.getElementById("ketchup").setAttribute("animation", "property: position; from: -0.5 2.2 0.55; to: -0.5 2 0.55; dur: 1000; easing: easeInOutElastic; loop: 3");
