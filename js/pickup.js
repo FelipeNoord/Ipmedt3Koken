d283900ac0b7b8dda82f3902c2c840d7560159d8
1108188 <s1108188@student.hsleiden.nl>
2022-01-28 18:35:36 +0100

    Knife animations and bread cut

--- js/pickup.js
+++ js/pickup.js
@@ -14,7 +14,8 @@
 
 //burger
       function addListeners(){
-        var j = 0;
+        var j = 3;
+        console.log(j);
         for (let i = 0; i < pickups.length; i++) {
         pickups[i].addEventListener("click", async function(evt){ //pickups is een array met alle elementen die als class "pickup" hebben
 
@@ -37,30 +38,57 @@
             j++;
          }
           if(pickups[i].id == "broodje" && j == 2){
-             document.getElementById("broodje").setAttribute("animation__pos", "property: position; from: -1 1.14 0.5; to: -0.5 1.17 0.55");
+            document.getElementById("broodje").setAttribute("animation__pos", "property: position; from: -0.5 1.17 0.55; to: 0.6 1.17 1");
+             
              
              j++;
           }
           if(pickups[i].id == "broodje" && j == 3){
-            document.getElementById("broodje").setAttribute("position", "-0.5 1.17 0.55");
+            document.getElementById("broodje").setAttribute("animation__pos", "property: position; from: -1 1.14 0.5; to: 0.618 1.34 0.6; dur: 2000;");
             document.getElementById("spotlight").setAttribute("light", "type: spot; intensity: 0.3; angle: 3; target: #slablaadje;")
+            document.getElementById("broodje").setAttribute("animation__rot", "property: rotation; from: 0 0 0; to: 0 0 90; delay: 500;");
+            setTimeout(broodjeWeg = (evt) => {
+              document.getElementById("broodje").setAttribute("visible", "false");
+              document.getElementById("broodje").setAttribute("scale", "0.01 0.01 0.01");
+              document.getElementById("gesnedenbroodje").setAttribute("visible", "true");
+            }, 2000);
             j++;
-          }if(pickups[i].id == "slablaadje" && j == 4){
+          } if(pickups[i].id == "mes" && j == 4){
+            document.getElementById("mes").setAttribute("animation__pos" , "property: position; from: 0.585 1.50 0.333; to: 0.585 1.25 0.333; dur: 1000");
+            document.getElementById("mes").setAttribute("animation__rot" , "property: rotation; from: 0 -90 0; to: 0 -90 0;");
+            document.getElementById("bovenkantbroodje").setAttribute("animation__pos", "property: position; from: 0 0 0; to: -0.28 0.66 0; delay: 1000");
+            document.getElementById("bovenkantbroodje").setAttribute("animation__rot", "property: rotation; from: 0 -90 0; to: 0 0 90; delay: 1000");
+            document.getElementById("onderkantbroodje").setAttribute("animation__pos", "property: position; from: 0 0 0; to: -0.5 -0.66 0; delay: 1000");
+            document.getElementById("onderkantbroodje").setAttribute("animation__rot", "property: rotation; from: 0 -90 0; to: 0 0 -90; delay: 1000");
+            document.getElementById("mes").setAttribute("animation__return", "property: position; from: 0.585 1.25 0.333; to: 0.146 1.155 0.475; delay: 1000;");
+            document.getElementById("mes").setAttribute("animation__retrot", "property: rotation; from: 0 -90 0; to: 90 -90 0; delay: 1000;");
+            // document.getElementById("mes").setAttribute("animation__pos" , "property: position; from: 0.8 1.25 0.3; to: 0.4 1.25 0.3; dur: 1000");
+            // document.getElementById("mes").setAttribute("animation__rot" , "property: rotation; from: 0 -90 30; to: 0 -90 0; loop: 4; dur: 250; easing: linear;");
+            // document.getElementById("mes").setAttribute("animation__return", "property: position; from: 0.4 1.25 0.3; to: 0.3 1.196 0.475; delay: 1000");
+            // document.getElementById("mes").setAttribute("animation__retrot", "property: rotation; from: 0 -90 0; to: 76 -72 -1; delay: 1000;")
+            j++
+          }
+          if(pickups[i].id == "gesnedenbroodje" && j == 5){
+            document.getElementById("gesnedenbroodje").setAttribute("animation__position", "property: position; from: 0.6 1.34 0.6; to: -0.7 1.307 0.55;");
+            j++;
+          }
+
+          if(pickups[i].id == "slablaadje" && j == 6){
             document.getElementById("slablaadje").setAttribute("position", "-0.5 1.2 0.55");
             document.getElementById("spotlight").setAttribute("light", "type: spot; intensity: 0.3; angle: 3; target: #tomaat;")
             j++;
-          }if(pickups[i].id == "tomaat"  && j == 5){
+          }if(pickups[i].id == "tomaat"  && j == 7){
             document.getElementById("tomaat").setAttribute("position", "0.6 1.17 0.6");
             j++;
-          }if(pickups[i].id == "tomaat"  && j == 6){
+          }if(pickups[i].id == "tomaat"  && j == 8){
             document.getElementById("tomaat").setAttribute("position", "-0.5 1.2 0.55");
             document.getElementById("spotlight").setAttribute("light", "type: spot; intensity: 0.3; angle: 3; target: #burger;")
             j++;
-          }if(pickups[i].id == "burger"  && j == 7){
+          }if(pickups[i].id == "burger"  && j == 9){
             document.getElementById("burger").setAttribute("position", "-0.5 1.2 0.55");
             document.getElementById("spotlight").setAttribute("light", "type: spot; intensity: 0.3; angle: 3; target: #ketchup;")
             j++;
-          }if(pickups[i].id == "ketchup"  && j == 8){
+          }if(pickups[i].id == "ketchup"  && j == 10){
             document.getElementById("spotlight").setAttribute("light", "type: spot; intensity: 0; angle: 3; target: #slablaadje;")
             document.getElementById("ketchup").setAttribute("position", "-0.5 2 0.55");
             document.getElementById("ketchup").setAttribute("rotation", "180 0 0");
