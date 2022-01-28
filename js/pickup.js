f7daac7d5d24436f5b1c6271719a158bb54f8bfb
tomlommerse <tomlommerse2001@gmail.com>
2022-01-28 01:23:19 +0100

    sequence

--- js/pickup.js
+++ js/pickup.js
@@ -15,14 +15,36 @@
 
 //burger
       function addListeners(){
-        
+        var j = 0;
         for (let i = 0; i < pickups.length; i++) {
         pickups[i].addEventListener("click", function(evt){ //pickups is een array met alle elementen die als class "pickup" hebben
-          if(pickups[i].id == "broodje"){
+          if(pickups[i].id == "burger" && j == 0){
+            document.getElementById("burger").setAttribute("position", "0.5 1.14 -1");
+            j++;
+         }
+          if(pickups[i].id == "broodje" && j == 1){
              document.getElementById("broodje").setAttribute("position", "0.6 1.17 0.6");
+             j++;
           }
-
-
+          if(pickups[i].id == "broodje" && j == 2){
+            document.getElementById("broodje").setAttribute("position", "-0.5 1.17 0.55");
+            j++;
+          }if(pickups[i].id == "slablaadje" && j == 3){
+            document.getElementById("slablaadje").setAttribute("position", "-0.5 1.2 0.55");
+            j++;
+          }if(pickups[i].id == "tomaat"  && j == 4){
+            document.getElementById("tomaat").setAttribute("position", "0.6 1.17 0.6");
+            j++;
+          }if(pickups[i].id == "tomaat"  && j == 5){
+            document.getElementById("tomaat").setAttribute("position", "-0.5 1.2 0.55");
+            j++;
+          }if(pickups[i].id == "burger"  && j == 6){
+            document.getElementById("burger").setAttribute("position", "-0.5 1.2 0.55");
+            j++;
+          }if(pickups[i].id == "ketchup"  && j == 7){
+            document.getElementById("ketchup").setAttribute("position", "-0.5 1.2 0.55");
+            j++;
+          }
       });
         }
       }
