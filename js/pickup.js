b7ac78ee140da42fa958a537a542d370241d4d05
1108188 <s1108188@student.hsleiden.nl>
2022-01-28 19:04:13 +0100

    text-speech-api

--- js/pickup.js
+++ js/pickup.js
@@ -6,15 +6,21 @@
         let pickups = document.getElementsByClassName("js--pickup");
         const camera = document.getElementById("js--camera");
         console.log("hallo");
+        const uiText = document.getElementById("js--uiText");
 
         function sleep(ms) {
           return new Promise(resolve => setTimeout(resolve, ms));
         }
 
 
+        const textToSpeech = (text) => {
+          responsiveVoice.speak(text.getAttribute("value"), "Dutch Male");
+        }
+
+        textToSpeech(uiText)
 //burger
       function addListeners(){
-        var j = 3;
+        var j = 0;
         console.log(j);
         for (let i = 0; i < pickups.length; i++) {
         pickups[i].addEventListener("click", async function(evt){ //pickups is een array met alle elementen die als class "pickup" hebben
@@ -23,8 +29,12 @@
             document.getElementById("olie").setAttribute("animation__pos", "property:position; from: 0.6 1.14 1.4;  to: 0.53 2 -1; dur: 3000; easing: linear;");
             document.getElementById("olie").setAttribute("animation__rot", " property: rotation; from: 0 0 0; to: 180 0 0; delay: 2000; dur: 1000;");
             document.getElementById("olie").setAttribute("animation__shake", "property: position; from: 0.53 2 -1; to: 0.53 2.2 -1; delay: 3000; dur: 2000; easing: easeInOutSine; loop: 3");
-            document.getElementById("spotlight").setAttribute("light", "type: spot; intensity: 0; angle: 3; target: #olie;")
+            document.getElementById("spotlight").setAttribute("light", "type: spot; intensity: 0; angle: 3; target: #olie;");
+            uiText.setAttribute("value" , "doe het brood op de plank");
+            textToSpeech(uiText);
             await sleep(10000);
+
+            
             
             document.getElementById("olie").setAttribute("position", "0.6 1.14 1.4");
              document.getElementById("olie").setAttribute("rotation", "0 0 0");
